7d3c4789792237c41430477c034361a7a76ec3ce
Ali Amaan <ali.amaan@psmorfia.com>
2022-07-30 14:03:18 +0500

    New Project

---
+++ src/users/pages/Users.js
@@ -0,0 +1,15 @@
+import {React} from "react";
+import UserList from "../components/UserList";
+
+const Users = () => {
+    const UsersData = [
+        {id:'a1', name:'Ali', image:'https://static.saltinourhair.com/wp-content/uploads/2019/04/23140026/things-to-do-venice-italy-header.jpg', places:5}
+    ]
+    return(
+        <>
+            <UserList items={UsersData}/>
+        </>
+    )
+}
+
+export default Users;
